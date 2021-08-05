72017038f0074f6623bf889e898e3a85d0bf750e
Nguoi An Phu <nguoianphu@gmail.com>
2021-08-05 09:08:44 +0700

    --dart-define [skip ci]

--- lib/src/helpers/constants.dart
+++ lib/src/helpers/constants.dart
@@ -1,5 +1,5 @@
 class Constant {
-  static const String newsApiKey = 'cab817200f92426bacb4edd2373e82ef';
+  static const String newsApiKey = String.fromEnvironment('NEWS_API_KEY');
   static const String baseUrl = 'https://newsapi.org/v2/';
   static const String topHeadLine = '/top-headlines';
 }
